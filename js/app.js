391c650e721a4fd24274810dd20c393f379867fc
Victor Resende Gualberto <89081017+Victor-vrg@users.noreply.github.com>
2024-01-04 11:53:26 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
+
 const paroquiaRoute_1 = __importDefault(require("./routes/paroquiaRoute"));
 const EventosRoute_1 = __importDefault(require("./routes/EventosRoute"));
 const ExcursaoRoute_1 = __importDefault(require("./routes/ExcursaoRoute"));
@@ -23,6 +23,7 @@ const tokenRoute_1 = __importDefault(require("./routes/tokenRoute"));
 const ServicoComunitarioRoute_1 = __importDefault(require("./routes/ServicoComunitarioRoute"));
 const mongo_1 = require("./database/mongo");
 require('dotenv').config();
+const cors = require('cors');
 const app = (0, express_1.default)();
 const port = 3001;
 app.use((0, cors_1.default)());
