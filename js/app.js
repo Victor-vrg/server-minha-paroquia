9fda69805db4d680c9b0b17c9646db2b5a5dd60f
Victor Resende Gualberto <89081017+Victor-vrg@users.noreply.github.com>
2024-01-04 11:59:33 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,7 +13,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-
 const paroquiaRoute_1 = __importDefault(require("./routes/paroquiaRoute"));
 const EventosRoute_1 = __importDefault(require("./routes/EventosRoute"));
 const ExcursaoRoute_1 = __importDefault(require("./routes/ExcursaoRoute"));
@@ -26,7 +25,7 @@ require('dotenv').config();
 const cors = require('cors');
 const app = (0, express_1.default)();
 const port = 3001;
-app.use((0, cors_1.default)());
+app.use(cors());
 app.use(express_1.default.json());
 const startServer = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
